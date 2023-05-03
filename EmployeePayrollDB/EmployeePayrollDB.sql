533dbd8414c0d48f9face61ff078e807dd5b51e2
shephali09 <hiremathshephali9@gmail.com>
2023-05-03 15:33:50 +0530

    Add: Ability to create employee payroll data in the payroll service database as part of CRUD Operation

--- EmployeePayrollDB/EmployeePayrollDB.sql
+++ EmployeePayrollDB/EmployeePayrollDB.sql
@@ -8,3 +8,9 @@ create table employee_payroll (id int not null auto_increment, name varchar(200)
 salary double not null, start_date date not null, primary key(id));
 
 describe employee_payroll;
+
+---- UC3 ----
+insert into employee_payroll(name,salary,start_date) values('Bill',10000.00,'2010-05-02');
+insert into employee_payroll(name,salary,start_date) values('Neeta',10000.00,'2010-05-02');
+insert into employee_payroll(name,salary,start_date) values('Shephali',2000.00,'2022-01-22');
+insert into employee_payroll(name,salary,start_date) values('Akash',3000.00,'2020-11-09');
